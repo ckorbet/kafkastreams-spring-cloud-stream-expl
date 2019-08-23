266cd335d858539515a0c04159776f4c47bd67f6
Carlos Torres <carlos.a.torres-gonzalez@capgemini.com>
2019-08-23 11:14:00 +0200

    Initial commit master branch

---
+++ src/main/java/com/cartorgon/ksscs/KafkastreamsSpringCloudStreamExplApplication.java
@@ -0,0 +1,11 @@
+package com.cartorgon.ksscs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkastreamsSpringCloudStreamExplApplication {
+	public static final void main(final String[] args) {
+		SpringApplication.run(KafkastreamsSpringCloudStreamExplApplication.class, args);
+	}
+}
